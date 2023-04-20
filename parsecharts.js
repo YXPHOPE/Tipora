f73474f334e3b94530c1479662280b6755f83911
YXP <2055979171@qq.com>
2023-04-20 20:00:06 +0800

    增加ChatMoss登录ChatGPT

--- parsecharts.js
+++ parsecharts.js
@@ -1,4 +1,16 @@
 var debugMd = true;
+if (!window.AIacnt) {
+  var AIacnt = [
+    // 向此数列中添加账户，当免登录字数用完后，将逐个使用本数组下的账户
+    // {email:'',password:''},
+  ];
+}
+var curAcnt = -1;
+if(!window.AItoken){
+  // token优先使用
+  var AItoken = [];
+}
+var curToken = -1;
 var cssStyle = `
 pre.md-meta-block.md-end-block,pre.md-fences.md-end-block.ty-contain-cm.modeLoaded[lang=style],pre.md-fences.md-end-block[lang=script] {
   padding: 5px;
@@ -76,10 +88,69 @@ div#typora-sidebar-resizer {
   display: block;
   transition: all 0.3s;
   cursor:grab;
+  left:0;width:20px;
 }
 #top-titlebar {height: 28px;}
 .active-tab-files #info-panel-tab-file .info-panel-tab-border, .active-tab-outline #info-panel-tab-outline div.info-panel-tab-border{background:rgba(100,100,100,0.8);}
 .file-node-content {color: #333;}
+.typora-sourceview-on #chat{display:block;}
+.typora-sourceview-on #typora-sidebar,.typora-sourceview-on #typora-sidebar-resizer{display:none !important;}
+#chat {
+  display:none;
+  position: absolute;
+  z-index: 100;
+  bottom: 0;
+  width: 100%;
+  min-width:200px;
+  text-align: center;
+}
+#aiarea {
+  width: 90%;
+  height: 100%;
+  padding: 5px 10px;
+  max-width: 900px;
+  margin: 0 5%;
+  border: 2px solid aqua;
+  border-radius: 8px;
+  transition: all 0.3s;
+}
+#aiarea:focus {
+  outline: 2px solid yellow;
+}
+#switch-pin-btn .ty-icon:before {content:"";}
+#AIlogin {
+  display:none;
+height: 100%;
+background: rgba(240,240,250,0.8);
+padding: 20px;
+text-align: center;
+border-radius: 8px;
+}
+#AIlogin tr td:first-child {
+  text-align: right;
+  width: 40%;
+}
+#AIlogin tr td:last-child {}
+#AIlogin span {
+  padding: 8px 20px;
+  margin: 0 20px;
+  background: #DDD;
+  border-radius: 6px;
+}
+.modal-login .modal-content {display:none;}
+.modal-login #AIlogin {display:block;}
+#AIlogin tr {
+  border: none;
+  background: none;
+}
+#AIlogin td {
+  border: none;
+}
+#AIlogin span:hover {
+  background: aqua;
+  cursor: pointer;
+}
+.modal-login {background: rgba(0,0,0,0.1);}
 /*  关闭所有动画（浪费） 建议在大文件中自行加上此句样式，防止卡顿（其实就是content重新排版造成的卡顿）
 html body * {transition: none !important;} */
 `;
@@ -131,6 +202,8 @@ var styleCustom = nE("style", "custom", 0);
 document.head.appendChild(styleCustom);
 var customScript = nE("script", "customScript", 0);
 document.head.appendChild(customScript);
+var mdfocus; // 当前焦点
+var Paste = $q("#context-menu-for-source").lastElementChild;
 
 function nE(e, id = undefined, cla = undefined, html = "") {
   var ele = document.createElement(e);
@@ -255,13 +328,16 @@ function parseCode(pre, nofocus = false) {
 
 // 粘贴示例代码，textarea可以多行，input不行
 const Cinput = document.createElement("textarea");
-function copyStr(str) {
+function copyStr(str, pa = 0) {
   // 不能设置display none，只能脱离文档流、全透明
   Cinput.style.cssText = "opacity:0;position:absolute;z-index:-1;";
   document.body.appendChild(Cinput);
   Cinput.value = str;
   Cinput.select();
-  Cdocument.execCommand("Copy");
+  document.execCommand("Copy");
+  if (pa) {
+    Paste.click();
+  }
 }
 
 function addStyle(css) {
@@ -336,7 +412,7 @@ function parseHTML(pre, nofocus = false) {
     pre.style.marginBottom = h + "px";
   }
 }
-function parseAll(){
+function parseAll() {
   let script = write.querySelector(".md-fences.md-end-block[lang=script] .CodeMirror-code");
   if (script) {
     document.documentElement.appendChild(nE("script", 0, 0, script.textContent));
@@ -473,7 +549,7 @@ h2 {
       }
       myTools();
       parseAll();
-      setTimeout(parseAll,2000);
+      setTimeout(parseAll, 2000);
     }
   }, 1000);
   // 每隔一段时间调用mytools 更新css、更新img名称
@@ -558,6 +634,104 @@ cursor: pointer;display: none;`;
       }, 2000);
     }
   });
+  var aiBoard = nE("div", "chat", 0, '<textarea id="aiarea" cols="60" rows="1"></textarea>');
+  var aiIn = aiBoard.firstChild;
+  document.body.appendChild(aiBoard);
+  const api = "http://43.154.117.92/api/chat-process?version=v1.5";
+  var header = {
+    Accept: "application/json, text/plain, */*",
+    // "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Code/1.76.2 Chrome/102.0.5005.196 Electron/19.1.11 Safari/537.36",
+    "Content-Type": "application/json",
+    // Origin: "vscode-webview://1nvg82joqdaj0ivg7ukh6rstfkkot6l06bh36mddovs1d49v1idh",
+    // "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en-US",
+    // "token":
+  };
+  // 响应结构 {"role":"assistant","id":"chatcmpl-76grWo3IJUipQO7f5oC5BDZVbPzM3","parentMessageId":"a99ae287-841a-4835-af9c-6b33ea5da240","text":"这","delta":"这","detail":{"id":"chatcmpl-76grWo3IJUipQO7f5oC5BDZVbPzM3","object":"chat.completion.chunk","created":1681829086,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":"这"},"index":0,"finish_reason":null}]}}
+  var data = { prompt: "Why 1+1 equals to 2\n 请详细回答", options: {}, apiKey: "" };
+  var loginPage = nE('div','AIlogin',0,`<div style="font: 700 20px sans-serif;">登录</div><table><tbody><tr><td style="width: 40%;">Email</td><td><input type="email"></td></tr><tr><td>Password</td><td><input type="password"></td></tr></tbody></table><div><span>Login</span><span>Cancel</span></div>`);
+  var modal = $q('#common-dialog');
+  modal.firstElementChild.appendChild(loginPage);
+  var loginInput = loginPage.querySelectorAll('input');
+  var spanBtn = loginPage.querySelectorAll('span');
+  spanBtn[0].addEventListener('click',()=>{
+    var data = {email:loginInput[0].value,password:loginInput[1].value};
+    loginToken(data);
+    hide(modal);
+    modal.classList.remove('modal-login');
+    modal.classList.remove('in');
+  })
+  spanBtn[1].addEventListener('click',()=>{
+    hide(modal);
+    modal.classList.remove('in');
+    modal.classList.remove('modal-login');
+  });
+  aiIn.addEventListener("keydown", (e) => {
+    var row = (aiIn.value + "11").split("\n").length;
+    aiIn.rows = row > 5 ? 5 : row;
+    if (e.key === "Enter" && e.ctrlKey) {
+      console.log(aiIn.value);
+      navigator.clipboard.writeText(aiIn.value);
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", api);
+      for (key in header) {
+        xhr.setRequestHeader(key, header[key]);
+      }
+      var start = 0,
+        end,
+        len = 0,
+        pre = 0;
+      xhr.onprogress = () => {
+        // var r = JSON.parse(xhr.responseText.slice(len));
+        var res = xhr.responseText;
+        start = res.lastIndexOf('"text":"') + 8;
+        end = res.lastIndexOf('","delta":"');
+        var text = res.slice(start, end);
+        if (text.length && len > pre) {
+          // console.log(pre,len,text.length);
+          copyStr(text.slice(pre).replace(/\\n/g, "\n").replace(/\\\\/g, "\\").replace(/\\"/g, '"'), 1);
+          pre = text.length;
+        }
+        len = text.length;
+      };
+      xhr.onload = () => {
+        var res = xhr.responseText;
+        start = res.lastIndexOf('"text":"');
+        start = start<0? 0:start+ 8;
+        end = res.lastIndexOf('","detail"');
+        var text = res.slice(start, end);
+        console.log(text);
+        if (pre < text.length) {
+          copyStr(text.slice(pre), 1);
+        }
+        if (xhr.responseText.length < 150) {
+          // 正常情况下，哪怕回复只有一个字，其数据总长度也超过了200，只有错误提示才有可能这么短
+          
+          if (curToken < AItoken.length-1) {
+            curToken++;
+            header["token"] = AItoken[curToken];
+          }
+          else if(curAcnt<AIacnt.length-1){
+            curAcnt++;
+            console.log(curAcnt,AIacnt);
+            loginToken(AIacnt[curAcnt]);
+          } 
+          else {
+            // 设计登录页面
+            modal.classList.add('modal-login');
+            modal.classList.add('in');
+            modal.style.display = 'block';
+          }
+        }
+      };
+      data.prompt = aiIn.value;
+      copyStr('\n\n<b style="color:red;">' + aiIn.value.split("\n")[0] + "</b>\n\n", 1);
+      xhr.send(JSON.stringify(data));
+      e.preventDefault();
+      e.stopPropagation();
+      return false;
+    }
+  });
   // sidebar 自动隐藏（两种外观模式下均有效）
   var sidebar = $q("#typora-sidebar");
   var resizer = $q("#typora-sidebar-resizer");
@@ -565,12 +739,14 @@ cursor: pointer;display: none;`;
   resizer.style.zIndex = 99999;
   var sidebarWidth = document.documentElement.style.getPropertyValue("--sidebar-width") || "255px";
   titlebar.style.left = sidebar.style.width = sidebarWidth;
-  var siderbarW = parseInt(sidebarWidth)-10;
   var content = $q("content");
 
-  function barOut() {
+  function barOut(e) {
+    if (e.clientX < 20) {
+      return false;
+    }
     sidebar.style.left = "-" + sidebarWidth;
-    resizer.style.cssText = "left:0;width:20px;";
+    resizer.style.display = "block";
     titlebar.style.left = 0;
     document.documentElement.style.setProperty("--siderbar-width", 0);
   }
@@ -587,22 +763,33 @@ cursor: pointer;display: none;`;
       }
     }
     sidebar.style.left = "0";
-    resizer.style.cssText = "width:0px;border-left:0px;left:"+sidebarWidth;
+    resizer.style.display = "none";
     titlebar.style.left = sidebarWidth;
     document.documentElement.style.setProperty("--siderbar-width", sidebarWidth);
   }
-  sidebar.addEventListener("mouseenter", () => {
-    barIn.call(sidebar);
-  });
-  sidebar.addEventListener("mouseleave", (e) => {
-    if (e.clientX > siderbarW) {
-    barOut();}
-  });
-  resizer.addEventListener("mouseenter", (e) => {
-    if (e.clientX > 30) {
-      return;
+  sidebar.addEventListener("mouseleave", barOut);
+  resizer.addEventListener("mouseenter", barIn);
+  // sidebar左下角添加固定取消固定按钮
+  var pin = `<div class="sidebar-footer-item footer-item-right footer-btn file-action-item not-empty-menu-group show" id="switch-pin-btn" ty-hint="固定/取消固定" aria-label="固定/取消固定">
+  <span class="switch-to-pin"><span class="ty-icon"><svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3528" width="12" height="12"><path d="M393.846154 64.174932l117.454119 0.399843H512.499805l117.454119-0.399843-10.395939 10.395939c-18.89262 18.89262-28.488872 44.982429-26.28973 71.472081l20.292073 250.502148c2.998829 36.68567 18.092932 72.171808 42.483405 99.661069l34.186646 38.684889-122.252245-0.899648-55.478329-0.399844h-0.99961l-55.478329 0.399844-122.452167 0.799687 34.186646-38.684888c24.390472-27.589223 39.484576-62.9754 42.483405-99.66107l20.292074-250.402187c2.199141-26.589613-7.397111-52.679422-26.289731-71.472081l-10.395939-10.395939m281.889887-64.174932h-0.199922L512.399844 0.599766h-0.799688L348.36392 0h-0.199922c-20.891839 0-39.684498 13.494729-45.582194 33.58688-4.098399 13.994533-1.899258 30.887934 17.79305 47.581414l38.584927 38.584927c5.597813 5.597813 8.39672 13.294807 7.796955 21.091761L346.464662 391.247169c-1.899258 23.190941-11.195627 45.08239-26.589613 62.475596l-61.87583 69.872706c-8.696603 9.796173-13.894572 22.291292-14.394377 35.386177-0.299883 8.996486 1.599375 18.89262 8.596642 27.28934 6.897306 8.296759 17.293245 12.894963 27.989067 12.894963h0.299882l175.931277-1.199532 55.178446 422.834831 0.399844 3.098789 0.399844-3.098789 55.178446-422.834831 175.931277 1.199532h0.299882c10.795783 0 21.191722-4.598204 27.989067-12.894963 6.897306-8.39672 8.796564-18.292854 8.596642-27.28934-0.399844-13.094885-5.697774-25.590004-14.394377-35.386177l-61.87583-69.872706c-15.393987-17.393206-24.690355-39.284654-26.589613-62.475596l-20.292074-250.402187c-0.599766-7.796954 2.199141-15.593909 7.796955-21.091761l38.584927-38.584927c19.692308-16.693479 21.891449-33.58688 17.79305-47.581414-5.997657-19.992191-24.790316-33.58688-45.682155-33.58688z" p-id="3529"></path></svg></span> </span>
+  <span class="switch-to-unpin"><span class="ty-icon"><svg width="12" height="12" xmlns="http://www.w3.org/2000/svg" alt="1"><g fill="#444" id="Layer_1"><path id="svg_1" p-id="3529" d="m4.64494,0.86429l1.34712,0.00458l0.01375,0l1.34712,-0.00458l-0.11923,0.11923c-0.21668,0.21668 -0.32675,0.51592 -0.30153,0.81975l0.23274,2.87311c0.03439,0.42076 0.20752,0.82777 0.48725,1.14306l0.3921,0.44369l-1.40216,-0.01031l-0.6363,-0.00458l-0.01146,0l-0.6363,0.00458l-1.40445,0.00917l0.3921,-0.44369c0.27974,-0.31644 0.45286,-0.72229 0.48725,-1.14306l0.23274,-2.87196c0.02522,-0.30496 -0.08484,-0.6042 -0.30153,-0.81975l-0.11923,-0.11923m3.23311,-0.73605l-0.00229,0l-1.87107,0.00688l-0.00917,0l-1.87222,-0.00688l-0.00229,0c-0.23962,0 -0.45515,0.15477 -0.5228,0.38522c-0.04701,0.1605 -0.02179,0.35427 0.20408,0.54573l0.44255,0.44255c0.0642,0.0642 0.0963,0.15248 0.08943,0.24191l-0.23274,2.87196c-0.02179,0.26599 -0.1284,0.51706 -0.30496,0.71656l-0.70968,0.8014c-0.09974,0.11236 -0.15936,0.25567 -0.1651,0.40586c-0.00344,0.10318 0.01835,0.21668 0.09859,0.313c0.07911,0.09516 0.19835,0.1479 0.32102,0.1479l0.00344,0l2.01783,-0.01375l0.63286,4.84966l0.00458,0.03554l0.00458,-0.03554l0.63286,-4.84966l2.01783,0.01375l0.00344,0c0.12382,0 0.24305,-0.05274 0.32102,-0.1479c0.07911,-0.0963 0.10089,-0.20981 0.09859,-0.313c-0.00458,-0.15019 -0.06535,-0.2935 -0.1651,-0.40586l-0.70968,-0.8014c-0.17656,-0.19949 -0.28319,-0.45057 -0.30496,-0.71656l-0.23274,-2.87196c-0.00688,-0.08943 0.02522,-0.17885 0.08943,-0.24191l0.44255,-0.44255c0.22586,-0.19146 0.25108,-0.38522 0.20408,-0.54573c-0.06879,-0.2293 -0.28433,-0.38522 -0.52395,-0.38522l0.00002,0l-0.00003,0z"/><rect x="-0.70069" y="5.61296" width="13.34542" height="0.63425" id="svg_3" rx="0.5" transform="rotate(-46, 5.97202, 5.93008)"/></g></svg></span></span>
+</div>`;
+  sidebar.querySelector("#ty-sidebar-footer>div").innerHTML += pin;
+  pin = sidebar.querySelector("#switch-pin-btn");
+  hide(pin.lastElementChild);
+  pin.addEventListener("click", () => {
+    pin.pin = !pin.pin;
+    if (pin.pin) {
+      sidebar.removeEventListener("mouseleave", barOut);
+      hide(pin.firstElementChild);
+      show(pin.lastElementChild);
+      write.style.marginLeft = sidebarWidth;
+    } else {
+      sidebar.addEventListener("mouseleave", barOut);
+      show(pin.firstElementChild);
+      hide(pin.lastElementChild);
+      write.style.margin = "0 auto";
     }
-    barIn.call(resizer, e);
   });
   // 软件右上角添加 导出和设置 按钮 （经典模式下无效）
   var btns = $q("#w-traffic-lights");
@@ -639,6 +826,31 @@ cursor: pointer;display: none;`;
   exhtml.onclick = () => {
     html.click();
   };
+  function show(e) {
+    e.style.display = "block";
+  }
+  function hide(e) {
+    e.style.display = "none";
+  }
+  function loginToken(data){
+    var login = new XMLHttpRequest();
+    login.open("POST", "http://43.154.117.92/luomacode-api/user/login");
+    login.setRequestHeader('Content-Type','application/json');
+    login.onload = () => {
+      if (login.responseText.indexOf('"msg":"successful"') > 0) {
+        header["token"] = login.responseText.slice(login.responseText.indexOf('Token":"') + 8, login.responseText.lastIndexOf('"}'));
+        var s = data instanceof Object ? data.email:'';
+        copyStr('\n登录成功：'+s,1);
+      } else {
+        alert("登录失败!\n" + login.responseText);
+      }
+    };
+    if(data instanceof Object){
+      data = JSON.stringify(data);
+    }
+    login.send(data);
+  }
 };
 
 initMD();
+// 需要追加当用尽了今天的50000字符上限后，更换token
